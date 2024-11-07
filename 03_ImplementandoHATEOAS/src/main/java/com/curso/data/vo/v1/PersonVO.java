0e6245a3dc17ccd04865493a40a05931b7477e7f
Andrey Mendonca <andrey.mmendonca@gmail.com>
2024-11-07 15:44:24 -0300

    Suporte ao HATEOAS implementado

--- 03_ImplementandoHATEOAS/src/main/java/com/curso/data/vo/v1/PersonVO.java
+++ 03_ImplementandoHATEOAS/src/main/java/com/curso/data/vo/v1/PersonVO.java
@@ -3,36 +3,43 @@ package com.curso.data.vo.v1;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class PersonVO implements Serializable{
+import org.springframework.hateoas.RepresentationModel;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.github.dozermapper.core.Mapping;
+
+@JsonPropertyOrder({"id","firstName","lastName","addresss","gender"})
+public class PersonVO extends RepresentationModel<PersonVO> implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-	private Long id;
-	
+	@Mapping("id")
+	@JsonProperty("id")
+	private Long key;
 	private String firstName;
-	
 	private String lastName;
-	
 	private String addresss;
 	private String gender;
 	
 	public PersonVO(){}
 	
-	public PersonVO(Long id, String firstName, String lastName, String addresss, String gender) {
+	public PersonVO(String firstName, String lastName, String addresss, String gender) {
 		super();
-		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.addresss = addresss;
 		this.gender = gender;
 	}
 
-	public Long getId() {
-		return id;
+	
+
+	public Long getKey() {
+		return key;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public void setKey(Long key) {
+		this.key = key;
 	}
 
 	public String getFirstName() {
@@ -69,24 +76,24 @@ public class PersonVO implements Serializable{
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(addresss, firstName, gender, id, lastName);
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + Objects.hash(addresss, firstName, gender, key, lastName);
+		return result;
 	}
 
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
-		if (obj == null)
+		if (!super.equals(obj))
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
 		PersonVO other = (PersonVO) obj;
 		return Objects.equals(addresss, other.addresss) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(gender, other.gender) && Objects.equals(id, other.id)
+				&& Objects.equals(gender, other.gender) && Objects.equals(key, other.key)
 				&& Objects.equals(lastName, other.lastName);
 	}
-
-	
-	
 
 }
--- 03_ImplementandoHATEOAS/src/main/java/com/curso/services/PersonService.java
+++ 03_ImplementandoHATEOAS/src/main/java/com/curso/services/PersonService.java
@@ -5,6 +5,10 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+import com.curso.controller.PersonController;
 import com.curso.data.vo.v1.PersonVO;
 import com.curso.exception.ResourceNotFoundException;
 import com.curso.mapper.DozerMapper;
@@ -18,18 +22,26 @@ public class PersonService {
 	private PersonRepository repository;
 	
 	public List<PersonVO> findAll(){
-		return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		List<PersonVO> vos = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
+		
+		vos.
+			stream()
+			.forEach(x -> x.add(linkTo(methodOn(PersonController.class).findById(x.getKey())).withSelfRel()));
+		return vos;
 	}
 	
 	public PersonVO findById(Long id) {
 		Person entity =  repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("PersonVO not found"));	
-		return DozerMapper.parseObject(entity, PersonVO.class);
+		PersonVO vo = DozerMapper.parseObject(entity, PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
+		return vo;
 	}
 	
 	public PersonVO create(PersonVO person) {
 		Person entity =  DozerMapper.parseObject(person, Person.class);
 		PersonVO vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
 	
@@ -38,10 +50,12 @@ public class PersonService {
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("PersonVO not found"));
 		
-		personVO.setId(entity.getId());
+		personVO.setKey(entity.getId());
 		
 		PersonVO vo = DozerMapper
 						.parseObject(repository.save(DozerMapper.parseObject(personVO, Person.class)), PersonVO.class);
+		
+		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 		
 	}
